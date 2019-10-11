aca01ee8cac331038660ca45df1b9a3153c58950
BennyLouie <bennylouie1412@gmail.com>
2019-10-11 13:27:08 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let win = array.find(e => {return e.result === 'W'})
+  if (win){
+    return win.year
+  }
+  else {
+    return undefined
+  }
+}
